c67ae14bfa8aee8083114bb7095adfdf55446c88
zsq337022 <zsq337022@163.com>
2020-11-30 07:48:48 +0800

    分享文件上传

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -75,32 +75,6 @@ export default {
 }
 </script>
 
-<style lang="stylus">
-.login-container
-  width 100%
-  min-height 100%
-  .login-form
-    width 520px
-    padding 160px 35px 0
-    margin 0 auto
-    overflow hidden
-  .title-container
-    text-align center
-    margin-bottom 20px
-    img
-      width 200px
-  .captcha-container
-    width 340px 
-    position relative
-    .el-button
-      width 90px
-      padding 0
-      line-height 40px
-    .captcha
-      position absolute
-      right -110px
-      img
-        width 90px
-        height 50px
-        cursor pointer
+<style>
+
 </style>
